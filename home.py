bc129a18446be87c5aa95f350acbfc466baeca09
butinhoni <guilherme.of.fernandes@gmail.com>
2025-01-10 10:40:33 -0400

    0.1
    Pagina inicial em construção

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config('Home | Grofer',layout='wide', page_icon= 'images/icon.png')
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.image('images/logo.png')
+#col4.title('Grofer APP')
+
+
+st.title('Website em Construção')
